34cb33009b968e965103ee85948aa391af23620b
GitHub Action <action@github.com>
2023-04-01 04:36:36 +0000

    Template cleanup

--- src/main/kotlin/com/github/cocainecong/fenbi/MyBundle.kt
+++ src/main/kotlin/com/github/cocainecong/fenbi/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.cocainecong.fenbi
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/cocainecong/fenbi/services/MyProjectService.kt
+++ src/main/kotlin/com/github/cocainecong/fenbi/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.cocainecong.fenbi.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.cocainecong.fenbi.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
